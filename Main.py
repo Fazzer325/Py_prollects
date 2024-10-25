c55e3c9c61f7a25e526ffef1d54a52c2e757a977
cris-pc <cristoramos16@hotmail.com>
2024-10-25 03:14:13 -0600

    a

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
 from Modulos.Colores import *
 
 print(f"Mi nombre es {cian("Cristo")} y yo {verde("Soy un Programador")}")
+
+Numeros=[1,2,3,4,5,6,7,8,9]
+
+def suma(*Numeros):
+    total=0
+    for i in Numeros:
+        total+=i
+
+    print(total)
+
+suma(*Numeros)
